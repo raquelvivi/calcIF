890c8a7870399679d3e45a1f93454065bf9406cc
raquelvivi <vivianraquelbatista@gmail.com>
2025-11-22 19:34:32 -0300

    resolvendo bo de compatibilidade nos navegadores

--- src/js/calculos.js
+++ src/js/calculos.js
@@ -1,60 +1,144 @@
-document.querySelectorAll("input").forEach(input => {
-    input.addEventListener("keydown", function (event) {
-        if (!/[0-9]/.test(event.key) && event.key !== "Backspace") {
-            event.preventDefault(); // Impede a entrada de caracteres não numéricos
-        }
-    });
+console.log("calculos.js LOADED");
+
+document.querySelectorAll("input").forEach((input) => {
+  input.addEventListener("keydown", function (event) {
+    if (!/[0-9]/.test(event.key) && event.key !== "Backspace") {
+      event.preventDefault(); // Impede a entrada de caracteres não numéricos
+    }
+  });
 });
 
-
 function calcular(tipo) {
+  let total = 0;
 
+  if (tipo == "P") {
+    let ul = document.getElementById("ul-pon");
 
-    let total = 0;
+    let totalList = [];
 
-    if (tipo == "P") {
-        let ul = document.getElementById("ul-pon");
+    total = 0;
+    let totalPond = 0;
 
-        let totalList = [];
+    for (let v = 0; v < ul.children.length; v++) {
+      const valorUs = ul.children[v].children[0].value;
 
-        total = 0;
-        let totalPond = 0;
+      const valeUs = ul.children[v].children[1].value;
 
-        for (let v = 0; v < ul.children.length; v++) {
+      totalList.push({ valor: valorUs, vale: valeUs });
 
-            const valorUs = ul.children[v].children[0].value;
-
-            const valeUs = ul.children[v].children[1].value;
-
-            totalList.push({ valor: valorUs, vale: valeUs });
-
-            totalPond = totalPond + parseInt(valeUs);
-
-        }
-
-        totalList.forEach(element => {
-            total = total + (element.valor * element.vale)
-        });
-
-        total = (total) / totalPond
-        total = parseInt(Math.round(total))
-
-
-    } else {
-        let ul = document.getElementById("ul-ari");
-
-        total = 0;
-
-        for (let v = 0; v < ul.children.length; v++) {
-
-            const valor = ul.children[v].children[0].value;
-
-
-            total = total + parseInt(valor)
-        }
+      totalPond = totalPond + parseInt(valeUs);
     }
 
-    const p = document.getElementById(`result${tipo}`);
+    totalList.forEach((element) => {
+      total = total + element.valor * element.vale;
+    });
+
+    total = total / totalPond;
+    total = parseInt(Math.round(total));
+  } else {
+    let ul = document.getElementById("ul-ari");
+
+    total = 0;
+
+    for (let v = 0; v < ul.children.length; v++) {
+      const valor = ul.children[v].children[0].value;
+
+      total = total + parseInt(valor);
+    }
+  }
+
+  const p = document.getElementById(`result${tipo}`);
+
+  resp = resposta(total);
+
+  p.innerText = resp.res;
+
+  p.style.opacity = 5;
+  p.style.color = resp.tipo;
+  p.style.backgroundColor = "rgba(0, 0, 0, 0.836)";
+
+  console.log(resp.res);
+}
+
+function resposta(total) {
+  const opcoes = [
+    { res: `você passou com media ${total}`, tipo: "var(--colorVerde)" },
+
+    {
+      res: `você reprovou com media ${total}\n Nem Recuperação resolve`,
+      tipo: "var(--colorRed)",
+    },
+
+    { res: `Socorro Nerd!!\n Media: ${total}`, tipo: "var(--colorVerde)" },
+
+    {
+      res: `você passou para a recuperação parabens :/ \n Media ${total}\n podia ser pior vai por mim`,
+      tipo: "var(--coloryelon)",
+    },
+
+    {
+      res: `você sobrevivel esse ano \n com media ${total} da recuperação`,
+      tipo: "var(--colorVerde)",
+    },
+
+    {
+      res: `algo errado, algo errado\nResultado: ${total}\nverifique os valores inseridos`,
+      tipo: "red",
+    },
+  ];
+
+  if (total <= 20) {
+    return opcoes[1];
+  }
+  if (total <= 59) {
+    return opcoes[3];
+  }
+  if (total <= 85) {
+    return opcoes[0];
+  }
+  if (total <= 100) {
+    return opcoes[2];
+  }
+  if (total > 100) {
+    return opcoes[5];
+  }
+}
+
+function calcularGe(tipo) {
+  let total = 0;
+
+  if (tipo == "B2") {
+    let b1 = document.getElementById("GRnota").value;
+
+    total = (60 * 5 - parseInt(b1) * 2) / 3;
+
+    const p = document.getElementById(`resultGR`);
+
+    p.innerText = `você precisa de ${parseInt(
+      Math.round(total)
+    )}\npara passar nessa materia`;
+
+    p.style.opacity = 5;
+    p.style.color = "var(--colorVerde)";
+  } else {
+    let total;
+
+    let G1 = document.getElementById("Gnota1").value;
+    let G2 = document.getElementById("Gnota2").value;
+    let G3 = document.getElementById("Gnota3").value;
+    let G4 = document.getElementById("Gnota4").value;
+
+    const lista = [G1, G2, G3, G4];
+
+    if (G3 && G4) {
+      total = (parseInt(G1) + parseInt(G2) + parseInt(G3) + parseInt(G4)) / 4;
+    } else {
+      total = (parseInt(G1) * 2 + parseInt(G2) * 3) / 5;
+    }
+
+    total = parseInt(Math.round(total));
+
+    const p = document.getElementById(`resultG`);
 
     resp = resposta(total);
 
@@ -62,106 +146,5 @@ function calcular(tipo) {
 
     p.style.opacity = 5;
     p.style.color = resp.tipo;
-    p.style.backgroundColor = "rgba(0, 0, 0, 0.836)";
-
-
-
-    console.log(resp.res);
-
-
+  }
 }
-
-
-
-
-function resposta(total) {
-
-
-    const opcoes = [
-        { "res": `você passou com media ${total}`, "tipo": "var(--colorVerde)" },
-
-        { "res": `você reprovou com media ${total}\n Nem Recuperação resolve`, "tipo": "var(--colorRed)" },
-
-        { "res": `Socorro Nerd!!\n Media: ${total}`, "tipo": "var(--colorVerde)" },
-
-        { "res": `você passou para a recuperação parabens :/ \n Media ${total}\n podia ser pior vai por mim`, "tipo": "var(--coloryelon)" },
-
-        { "res": `você sobrevivel esse ano \n com media ${total} da recuperação`, "tipo": "var(--colorVerde)" },
-
-        { "res": `algo errado, algo errado\nResultado: ${total}\nverifique os valores inseridos`, "tipo": "red" },
-    ];
-
-    if (total <= 20) {
-        return opcoes[1];
-    };
-    if (total <= 59) {
-        return opcoes[3]
-    };
-    if (total <= 85) {
-        return opcoes[0]
-    };
-    if (total <= 100) {
-        return opcoes[2]
-    };
-    if (total > 100) {
-        return opcoes[5]
-    };
-
-}
-
-
-function calcularGe(tipo) {
-
-    let total = 0
-
-    if (tipo == "B2") {
-
-        let b1 = document.getElementById("GRnota").value;
-
-        total = ((60 * 5) - (parseInt(b1) * 2)) / 3
-
-        const p = document.getElementById(`resultGR`);
-
-        p.innerText = `você precisa de ${parseInt(Math.round(total))}\npara passar nessa materia`;
-
-        p.style.opacity = 5;
-        p.style.color = "var(--colorVerde)";
-
-    } else {
-        let total;
-
-        let G1 = document.getElementById("Gnota1").value;
-        let G2 = document.getElementById("Gnota2").value;
-        let G3 = document.getElementById("Gnota3").value;
-        let G4 = document.getElementById("Gnota4").value;
-
-        const lista = [G1, G2, G3, G4];
-
-
-            
-            if (G3 && G4) {
-
-                total = (parseInt(G1) + parseInt(G2) + parseInt(G3) + parseInt(G4)) / 4;
-
-            } else {
-                total = ((parseInt(G1) * 2) + (parseInt(G2) * 3)) / 5;
-            }
-
-            total = parseInt(Math.round(total));
-
-            const p = document.getElementById(`resultG`);
-
-            resp = resposta(total);
-
-            p.innerText = resp.res;
-
-            p.style.opacity = 5;
-            p.style.color = resp.tipo;
-
-
-        }
-
-
-
-    }
-
--- src/js/muda.js
+++ src/js/muda.js
@@ -1,162 +1,136 @@
+console.log("muda.js LOADED");
 
-document.querySelectorAll("input").forEach(input => { // seleciona todos os section dinamicamente
-    input.value = "";
+document.querySelectorAll("input").forEach((input) => {
+  // seleciona todos os section dinamicamente
+  input.value = "";
 });
 
+function selecaoSelect() {
+  // select das opçoes (bimestral, geral e recuperação)
 
-function selecaoSelect() { // select das opçoes (bimestral, geral e recuperação)
+  click = 1;
+  const p = document.getElementById(`resultP`);
+  const p2 = document.getElementById(`resultA`);
+  p.style.opacity = 0;
+  p2.style.opacity = 0;
 
-    click = 1;
-    const p = document.getElementById(`resultP`);
-    const p2 = document.getElementById(`resultA`);
-    p.style.opacity = 0;
-    p2.style.opacity = 0;
+  const valorOpcao = document.getElementById("opcoes");
 
-    
-    const valorOpcao = document.getElementById("opcoes");
+  const sectionSelec = document.getElementById(`${valorOpcao.value}`);
 
-    const sectionSelec = document.getElementById(`${valorOpcao.value}`);
+  console.log(valorOpcao);
+  document.querySelectorAll("section").forEach((section) => {
+    // seleciona todos os section dinamicamente
+    section.style.display = "none";
+  });
 
-
-    document.querySelectorAll("section").forEach(section => { // seleciona todos os section dinamicamente
-        section.style.display = "none";
-    });
-
-    sectionSelec.style.display = "block";
-
+  sectionSelec.style.display = "block";
 }
 var click = 1;
 
-function selecaoTipo() { // select tipo ponderado e aritimetica
+function selecaoTipo() {
+  // select tipo ponderado e aritimetica
 
-    click = 1;
+  click = 1;
 
-    const valorOpcao = document.getElementById("calculo");
-    const p = document.getElementById(`resultP`);
-    const p2 = document.getElementById(`resultA`);
+  const valorOpcao = document.getElementById("calculo");
+  const p = document.getElementById(`resultP`);
+  const p2 = document.getElementById(`resultA`);
 
-    const sectionSelec = document.getElementById(`${valorOpcao.value}`);
+  const sectionSelec = document.getElementById(`${valorOpcao.value}`);
 
-    const div = document.getElementsByClassName("divBimes") // seleciona todos com clase de divBimes dinamicamente
+  const div = document.getElementsByClassName("divBimes"); // seleciona todos com clase de divBimes dinamicamente
 
-    div.pon.style.display = "none";
-    div.ari.style.display = "none";
-    p.style.opacity = 0;
-    p2.style.opacity = 0;
+  div.pon.style.display = "none";
+  div.ari.style.display = "none";
+  p.style.opacity = 0;
+  p2.style.opacity = 0;
 
-
-    sectionSelec.style.display = "block";
+  sectionSelec.style.display = "block";
 }
 
-
-
 function AddInput(tipo) {
+  click = click + 1;
 
-    click = click + 1;
+  let ul;
 
+  let li = document.createElement("li");
+  let input = document.createElement("input");
 
-    let ul;
+  input.setAttribute("placeholder", ` ${click}° Nota `);
+  input.setAttribute("type", `number`);
 
+  li.appendChild(input);
 
-    let li = document.createElement("li");
-    let input = document.createElement("input");
+  let p;
 
+  if (tipo == "pon") {
+    p = document.getElementById("resultP");
 
-    input.setAttribute("placeholder", ` ${click}° Nota `);
-    input.setAttribute("type", `number`);
+    ul = document.getElementById("ul-pon");
 
-    li.appendChild(input);
+    input.setAttribute("id", `BPnota${click}`);
 
-    let p ;
+    let inputPon = document.createElement("input");
+    inputPon.setAttribute("placeholder", `vale?`);
+    inputPon.setAttribute("class", `part`);
+    inputPon.setAttribute("type", `number`);
 
-    if (tipo == "pon") {
+    li.appendChild(inputPon);
+  } else {
+    p = document.getElementById("resultA");
+    input.setAttribute("id", `BAnota${click}`);
+    ul = document.getElementById("ul-ari");
+  }
 
-        p = document.getElementById("resultP");
-
-        ul = document.getElementById("ul-pon");
-
-        input.setAttribute("id", `BPnota${click}`);
-
-        let inputPon = document.createElement("input");
-        inputPon.setAttribute("placeholder", `vale?`);
-        inputPon.setAttribute("class", `part`);
-        inputPon.setAttribute("type", `number`);
-
-        li.appendChild(inputPon);
-
-    } else {
-        p = document.getElementById("resultA");
-        input.setAttribute("id", `BAnota${click}`);
-        ul = document.getElementById("ul-ari");
-    }
-
-    if (ul.children.length < 10) {
-        ul.appendChild(li);
-    }else{
-        click--;
-        p.innerText = "no maximo 10";
-        p.style.opacity = 5;
-        p.style.color = "white";
-
-
-    }
-    
-
+  if (ul.children.length < 10) {
+    ul.appendChild(li);
+  } else {
+    click--;
+    p.innerText = "no maximo 10";
+    p.style.opacity = 5;
+    p.style.color = "white";
+  }
 }
 
 function DelInput(tipo) {
+  let ul;
 
-    let ul;
+  if (tipo == "pon") {
+    ul = document.getElementById("ul-pon");
+  } else {
+    ul = document.getElementById("ul-ari");
+  }
 
-    if (tipo == "pon") {
-        ul = document.getElementById("ul-pon");
-    } else {
-        ul = document.getElementById("ul-ari");
-    }
-
-    // Verifica se há pelo menos um <li> na lista antes de remover
-    if (ul.children.length > 1) {
-        click = click - 1;
-        ul.removeChild(ul.lastChild);
-    }
-
+  // Verifica se há pelo menos um <li> na lista antes de remover
+  if (ul.children.length > 1) {
+    click = click - 1;
+    ul.removeChild(ul.lastChild);
+  }
 }
 
 function AbrirIntegrado() {
+  let div = document.getElementById("divGeral");
+  let Gnota3 = document.getElementById("Gnota3");
+  let Gnota4 = document.getElementById("Gnota4");
 
-    let div = document.getElementById("divGeral");
-    let Gnota3 = document.getElementById("Gnota3");
-    let Gnota4 = document.getElementById("Gnota4");
-
-    if (div.style.display == "block") {
-
-        div.style.display = "none";
-        Gnota3.value = "";
-        Gnota4.value = "";
-
-    } else {
-        div.style.display = "block";
-    }
-
-
-
+  if (div.style.display == "block") {
+    div.style.display = "none";
+    Gnota3.value = "";
+    Gnota4.value = "";
+  } else {
+    div.style.display = "block";
+  }
 }
 
 function AbrirNota2b() {
+  let div = document.getElementById("nota2b");
+  let GRnota = document.getElementById("GRnota");
 
-    let div = document.getElementById("nota2b");
-    let GRnota = document.getElementById("GRnota");
-
-    if (div.style.display == "block") {
-
-        div.style.display = "none";
-        GRnota.value = 0;
-
-    } else {
-        div.style.display = "block";
-    }
-
-
+  if (div.style.display == "block") {
+    div.style.display = "none";
+    GRnota.value = 0;
+  } else {
+    div.style.display = "block";
+  }
 }
-
-
